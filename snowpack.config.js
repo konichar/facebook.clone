e1b44cde5447407e6c1f33f5212b61232c43529d
reechee <richardokonicha@gmail.com>
2021-04-04 09:57:10 +0100

    setup snowpack buildtool cra build tool

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,25 +1,14 @@
 module.exports = {
-    extends: '@snowpack/app-scripts-react',
-
-    devOptions: {
-        port: 3000,
-        src: 'src',
-        bundle: false,
-        routes: "public/index.html",
-    },
-
-    packageOptions: {
-        polyfillNode: true,
-        rollup: {
-            plugins: [require("rollup-plugin-node-polyfills")()],
-          },
-    },
-    
-    plugins: [
-    [
-        '@snowpack/plugin-build-script',
-        { cmd: 'postcss $FILE', input: ['.css'], output: ['.css']}
-    ]
-    ],
-    
-}
+  extends: "@snowpack/app-scripts-react",
+  mount: {
+    public: { url: "/" },
+    src: { url: "/src" },
+  },
+  devOptions: {
+    port: 3000,
+    // src: '/src',
+    bundle: false,
+    routes: "public/index.html",
+  },
+  packageOptions: {},
+};
